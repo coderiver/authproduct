7d7358eebea621d5c27aec4616782458806387a3
etna-scorpio <etna.scorpio@gmail.com>
2015-05-25 15:35:39 +0300

    responsive on 768 started

--- js/common.js
+++ js/common.js
@@ -16,13 +16,13 @@ head.ready(function() {
 	// });
 
 	//fullpage
-	if ($('.fullpage').length > 0){
-		$('.fullpage').fullpage({
+	if ($('.fullpage').length > 0 && $(window).width() > 768 && $(window).height() > 685){
+		var fullPage = $('.fullpage').fullpage({
 			scrollingSpeed: 800,
 			verticalCentered: false,
 			fixedElements: '.header',
 			anchors: ['main', 'products', 'blog', 'about', 'all'],
-			responsive: 768,
+			responsive: 769,
 
 
 			onLeave: function(index, nextIndex, direction){
@@ -59,6 +59,7 @@ head.ready(function() {
 			}
 		});
 	}
+
 
 	$('.nav__link').click(function(e){
 		e.preventDefault;
@@ -143,5 +144,92 @@ head.ready(function() {
 		adaptiveHeight: true,
 		dots: true
 	});
+
+	$('.blog').slick({
+		arrows: false,
+		slidesToShow: 3,
+		responsive: [
+			{
+			  breakpoint: 1028,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				arrows: true
+			  }
+			},
+			{
+			  breakpoint: 768,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: true
+			  }
+			}
+		]
+	});
+
+	//slick slider inner
+	var total = $('.gallery__item').length;
+
+	$('.js-slick').slick({
+	  infinite: true,
+	  arrows: true,
+	  slidesToShow: 1
+	 });
+	$('.gallery__total').text(total);
+
+	$('.js-slick').on('afterChange', function(event, slick, currentSlide, nextSlide){
+		$('.gallery__current').text($('.slick-active').index());
+	});
+
+	$('.gallery__prev').on('click', function(){
+		$('.slick-prev').trigger('click');
+		return false;
+	 });
+
+	$('.gallery__next').on('click', function(){
+		$('.slick-next').trigger('click');
+		return false;
+	});
+
+	function number() {
+		var number = $(".js-number");
+		number.each(function(){
+			var max_number = +($(this).attr("data-max-number"));
+			var input = $(this).find("input");
+			var plus = $(this).find(".js-plus-number");
+			var minus = $(this).find(".js-minus-number");
+			plus.on("click", function(){
+				var val = +(input.val());
+				if (val >= max_number) {
+					return false
+				}
+				else {
+					val += 1;
+					input.val(val);
+				}
+			});
+			minus.on("click", function(){
+				var val = +(input.val());
+				if (val > 1) {
+					val -= 1;
+					input.val(val);
+				}
+				return false;
+			});
+			input.on("change", function(){
+				var val = +$(this).val();
+				if (val > max_number) {
+					val = max_number;
+					$(this).val(val);
+				}
+				if (val == '') {
+					val = 1;
+					$(this).val(val);
+				}
+			});
+		});
+	}
+	number();
 
 });
