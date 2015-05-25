0cc81c6ec53f566d1a1dc9660530ab93d4826800
etna-scorpio <etna.scorpio@gmail.com>
2015-05-25 18:33:21 +0300

    svg.js and popups added

--- js/common.js
+++ js/common.js
@@ -1,9 +1,5 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	//	$(".js-popup").hide();
-	// });
-
 	// function scrollFixedElements() {
 	//		 var scroll_left = $(this).scrollLeft();
 	//		 $(".fixed-element").css({
@@ -86,144 +82,6 @@ head.ready(function() {
 		e.preventDefault;
 	});
 
-	//svg menu btn animation
-	(function() {
-
-		function SVGHamburger( el, options ) {
-			this.el = el;
-			this.init();
-		}
-
-		SVGHamburger.prototype.init = function() {
-			this.shapeEl = this.el.querySelector( 'span.morph-shape' );
-
-			var s = Snap( this.shapeEl.querySelector( 'svg' ) );
-			this.pathEl1 = s.select( 'path:nth-of-type(1)' );
-			this.pathEl2 = s.select( 'path:nth-of-type(2)' );
-			this.paths = {
-				reset : {
-					path1 : this.pathEl1.attr( 'd' ),
-					path2 : this.pathEl2.attr( 'd' )
-				},
-				open : this.shapeEl.getAttribute( 'data-morph-open' ).split( ';' ),
-				close : this.shapeEl.getAttribute( 'data-morph-close' ).split( ';' )
-			};
-
-			this.isOpen = false;
-
-			this.initEvents();
-		};
-
-		SVGHamburger.prototype.initEvents = function() {
-			this.el.addEventListener( 'mouseover', this.toggleShow.bind(this) );
-			this.el.addEventListener( 'mouseout', this.toggleHide.bind(this) );
-		};
-
-		SVGHamburger.prototype.toggleShow = function() {
-			var self = this,
-				paths = this.isOpen ? this.paths.close : this.paths.open;
-
-				setTimeout( function() { classie.add( self.el, 'is-open' ); }, 200 );
-
-			this.pathEl1.stop().animate( { 'path' : paths[0] }, 300, mina.easeout, function() {
-				self.pathEl1.stop().animate( { 'path' : self.paths.reset.path1 }, 800, mina.elastic );
-			} );
-			this.pathEl2.stop().animate( { 'path' : paths[1] }, 300, mina.easeout, function() {
-				self.pathEl2.stop().animate( { 'path' : self.paths.reset.path2 }, 800, mina.elastic );
-			} );
-
-			this.isOpen = !this.isOpen;
-		};
-
-
-		SVGHamburger.prototype.toggleHide = function() {
-			var self = this,
-				paths = this.isOpen ? this.paths.close : this.paths.open;
-
-				setTimeout( function() { classie.remove( self.el, 'is-open' ); }, 200 );
-
-			this.pathEl1.stop().animate( { 'path' : paths[0] }, 300, mina.easeout, function() {
-				self.pathEl1.stop().animate( { 'path' : self.paths.reset.path1 }, 800, mina.elastic );
-			} );
-			this.pathEl2.stop().animate( { 'path' : paths[1] }, 300, mina.easeout, function() {
-				self.pathEl2.stop().animate( { 'path' : self.paths.reset.path2 }, 800, mina.elastic );
-			} );
-
-			this.isOpen = !this.isOpen;
-		};
-
-		new SVGHamburger( document.getElementById('hamburger'));
-
-	})();
-
-	// buttons animations
-
-	(function() {
-
-		function extend( a, b ) {
-			for( var key in b ) {
-				if( b.hasOwnProperty( key ) ) {
-					a[key] = b[key];
-				}
-			}
-			return a;
-		}
-
-		function SVGButton( el, options ) {
-			this.el = el;
-			this.options = extend( {}, this.options );
-			extend( this.options, options );
-			this.init();
-		}
-
-		SVGButton.prototype.options = {
-			speed : { reset : 800, active : 150 },
-			easing : { reset : mina.elastic, active : mina.easein }
-		};
-
-		SVGButton.prototype.init = function() {
-			this.shapeEl = this.el.querySelector( 'span.morph-shape' );
-
-			var s = Snap( this.shapeEl.querySelector( 'svg' ) );
-			this.pathEl = s.select( 'path' );
-			this.paths = {
-				reset : this.pathEl.attr( 'd' ),
-				active : this.shapeEl.getAttribute( 'data-morph-active' )
-			};
-
-			this.initEvents();
-		};
-
-		SVGButton.prototype.initEvents = function() {
-			this.el.addEventListener( 'mouseover', this.down.bind(this) );
-			this.el.addEventListener( 'touchstart', this.down.bind(this) );
-
-			this.el.addEventListener( 'touchend', this.up.bind(this) );
-
-			this.el.addEventListener( 'mouseout', this.up.bind(this) );
-		};
-
-		SVGButton.prototype.down = function() {
-			this.pathEl.stop().animate( { 'path' : this.paths.active }, this.options.speed.active, this.options.easing.active );
-		};
-
-		SVGButton.prototype.up = function() {
-			this.pathEl.stop().animate( { 'path' : this.paths.reset }, this.options.speed.reset, this.options.easing.reset );
-		};
-
-		[].slice.call( document.querySelectorAll( 'button.button--effect-1' ) ).forEach( function( el ) {
-			new SVGButton( el );
-		} );
-
-		[].slice.call( document.querySelectorAll( 'button.button--effect-2' ) ).forEach( function( el ) {
-			new SVGButton( el, {
-				speed : { reset : 650, active : 650 },
-				easing : { reset : mina.elastic, active : mina.elastic }
-			} );
-		} );
-
-	})();
-
 	//slick slider
 	$('.slick-product').slick({
 		infinite: true,
@@ -244,7 +102,7 @@ head.ready(function() {
 		]
 	});
 
-	$('.blog').slick({
+	$('.js-blog').slick({
 		arrows: false,
 		slidesToShow: 3,
 		responsive: [
@@ -291,6 +149,7 @@ head.ready(function() {
 		return false;
 	});
 
+	//plus/minus
 	function number() {
 		var number = $(".js-number");
 		number.each(function(){
@@ -374,12 +233,38 @@ head.ready(function() {
 				agree: "required"
 			},
 		 	messages: {
-			   required: {
+				required: {
 				required: "",
 				minlength: ""
 				}
 			}
 		});
 	}
+
+	//popups
+	$('.js-popup-btn').on('click', function() {
+
+		$('.js-popup').addClass('is-active');
+		return false;
+
+	 });
+	 $('.js-popup-close').on('click', function() {
+
+		$('.js-popup').removeClass('is-active');
+		return false;
+
+	 });
+
+	 $('.js-popup').each(function() {
+
+		$('body').on('click', function () {
+		 $('.js-popup').removeClass('is-active');
+		});
+
+		$('.popup__inner').on('click', function(event) {
+		 event.stopPropagation();
+		});
+
+	 });
 
 });
--- js/init.js
+++ js/init.js
@@ -4,4 +4,5 @@ head.load("js/lib/jquery.js",
 					"js/lib/snap.js",
 					"js/lib/classie.js",
 					"js/lib/jquery.validate.min.js",
+					"js/lib/svg-animate.js",
 					"js/common.js");
