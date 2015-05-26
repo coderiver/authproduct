c29baee6b46a2cd00dd505da6863ca3ee0499b1e
whats0n <mrdvv@mail.ru>
2015-05-26 15:15:18 +0300

    almost

--- js/init.js
+++ js/init.js
@@ -3,6 +3,6 @@ head.load("js/lib/jquery.js",
 					"js/lib/scrollTo.min.js",
 					"js/lib/snap.js",
 					"js/lib/classie.js",
-					"js/lib/jquery.validate.min.js",
+					"js/lib/jquery.form-validator.min.js",
 					"js/lib/svg-animate.js",
 					"js/common.js");
